ef151e749e5e75402e89e4909a02e9001d9d86e4
ivanAgustinSDEV <109189769+ivanAgustinSDEV@users.noreply.github.com>
2022-07-21 19:14:39 -0300

    Add files via upload

---
+++ src/app/data/constants/carousel.const.ts
@@ -0,0 +1,54 @@
+import {ICarouselItem} from '@shared/components/carousel/Icarousel-item.metadata';
+
+export const CAROUSEL_DATA_ITEMS: ICarouselItem[] = [
+    {
+        id: 1,
+        title: {
+            first: 'Mendoza',
+            second: 'ARGENTINA'
+        },
+        subtitle: 'Paquetes, alojamientos y mas!',
+        link: '/',
+        image: 'assets/images/1.jpg'
+    },
+    {
+        id: 2,
+        title: {
+            first: 'Bariloche',
+            second: 'ARGENTINA'
+        },
+        subtitle: 'Paquetes, alojamientos y mas!',
+        link: '/',
+        image: 'assets/images/2.jpg'
+    },
+    {
+        id: 3,
+        title: {
+            first: 'Salta',
+            second: 'ARGENTINA'
+        },
+        subtitle: 'Paquetes, alojamientos y mas!',
+        link: '/',
+        image: 'assets/images/3.jpg'
+    },
+    {
+        id: 4,
+        title: {
+            first: 'Buenos Aires',
+            second: 'ARGENTINA'
+        },
+        subtitle: 'Paquetes, alojamientos y mas!',
+        link: '/',
+        image: 'assets/images/4.jpg'
+    },
+    {
+        id: 5,
+        title: {
+            first: 'Jujuy',
+            second: 'ARGENTINA'
+        },
+        subtitle: 'Paquetes, alojamientos y mas!',
+        link: '/',
+        image: 'assets/images/5.jpg'
+    }
+];
---
+++ src/app/data/constants/users.const.ts
@@ -0,0 +1,55 @@
+import { ICardUser } from '@shared/components/cards/card-user/icard-user.metadata';
+
+export const USERS_DATA: ICardUser[] = [
+    {
+      id: 1,
+      name: 'Ivan Larios',
+      age: 27,
+      description: 'Desarrollador fullstack',
+      avatar: '',
+      work: 'Project manager',
+    },
+    {
+      id: 2,
+      name: 'Lucia Morales',
+      age: 19,
+      description: 'Diseñador',
+      avatar: ''
+    },
+    {
+      id: 3,
+      name: 'Maria Suarez',
+      age: 27,
+      description: 'Desarrollador back',
+      avatar: ''
+    },
+    {
+      id: 4,
+      name: 'Hector Larios',
+      age: 20,
+      description: 'Desarrollador front',
+      avatar: ''
+    },
+    {
+      id: 5,
+      name: 'Martin Suñiga',
+      age: 19,
+      description: 'Diseñador',
+      avatar: ''
+    },
+    {
+      id: 6,
+      name: 'Francisco Lopez',
+      age: 27,
+      description: 'Desarrollador back',
+      avatar: ''
+    },
+    {
+      id: 7,
+      name: 'Lucia Montalvo',
+      age: 20,
+      description: 'Desarrollador front',
+      // tslint:disable-next-line: max-line-length
+      avatar: ''
+    },
+];
